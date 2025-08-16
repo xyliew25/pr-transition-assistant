aa73498877eb5b41ca6646fdfa458e6e3625d980
xyliew25 <e0550381@u.nus.edu>
2025-08-16 15:21:42 +0800

    Import Chroma from langchain_chroma

--- rag.py
+++ rag.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
